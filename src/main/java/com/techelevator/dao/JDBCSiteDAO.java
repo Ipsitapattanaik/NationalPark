96dccaf27c4eee3e15e2c6522f8ea8ef0cc261d5
Arthur Mueller <AWM21@pitt.edu>
2019-02-21 17:34:52 -0500

    Finished JDBCSiteDAO.java

--- src/main/java/com/techelevator/dao/JDBCSiteDAO.java
+++ src/main/java/com/techelevator/dao/JDBCSiteDAO.java
@@ -23,29 +23,54 @@ public class JDBCSiteDAO implements SiteDAO {
 	@Override
 	public List<Site> getAvailableSitesByCampground(long campground_id, LocalDate start_date, LocalDate end_date) {
 		List<Site> sites = new ArrayList<>();
-String sqlGetAvailableSitesByCampground = "SELECT * "+
-											"FROM site "+
-											"WHERE campground_id = ? ";
-SqlRowSet results =jdbcTemplate.queryForRowSet(sqlGetAvailableSitesByCampground, campground_id);
-		while(results.next()) {
+		String sqlGetAvailableSitesByCampground = "SELECT * " + "FROM site " + "WHERE campground_id = ? ";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlGetAvailableSitesByCampground, campground_id);
+		while (results.next()) {
 			Site theSite = mapRowToSite(results);
-			String sqlGetReservations = "SELECT * "+
-										"FROM reservation "+
-										"Where site_id = ?";
-			SqlRowSet results2=jdbcTemplate.queryForRowSet(sqlGetReservations, theSite.getId());
-			while(results2.next()) {
+			String sqlGetReservations = "SELECT * " + "FROM reservation " + "Where site_id = ?";
+			SqlRowSet results2 = jdbcTemplate.queryForRowSet(sqlGetReservations, theSite.getId());
+			boolean booked = false;
+			while (results2.next()) {
 				Reservation theReservation = mapRowToReservation(results2);
+				if (theReservation.getFromDate().isAfter(start_date) && theReservation.getFromDate().isBefore(end_date)
+						|| theReservation.getToDate().isAfter(start_date)
+								&& theReservation.getToDate().isBefore(end_date)) {
+					booked = true;
+				}
+			}
+			if (!booked) {
+				sites.add(theSite);
 			}
 		}
-											
-											
-											return sites;
+
+		return sites;
 	}
 
 	@Override
 	public List<Site> getAvailableSitesByPark(long park_id, LocalDate start_date, LocalDate end_date) {
 		List<Site> sites = new ArrayList<>();
-		
+		String sqlGetAvailableSitesByPark = "SELECT * "
+				+ "FROM site JOIN campground ON site.campground_id=campground.campground_id"
+				+ "WHERE campground.park_id = ? ";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlGetAvailableSitesByPark, park_id);
+		while (results.next()) {
+			Site theSite = mapRowToSite(results);
+			String sqlGetReservations = "SELECT * " + "FROM reservation " + "Where site_id = ?";
+			SqlRowSet results2 = jdbcTemplate.queryForRowSet(sqlGetReservations, theSite.getId());
+			boolean booked = false;
+			while (results2.next()) {
+				Reservation theReservation = mapRowToReservation(results2);
+				if (theReservation.getFromDate().isAfter(start_date) && theReservation.getFromDate().isBefore(end_date)
+						|| theReservation.getToDate().isAfter(start_date)
+								&& theReservation.getToDate().isBefore(end_date)) {
+					booked = true;
+				}
+			}
+			if (!booked) {
+				sites.add(theSite);
+			}
+		}
+
 		return sites;
 	}
 
@@ -60,12 +85,15 @@ SqlRowSet results =jdbcTemplate.queryForRowSet(sqlGetAvailableSitesByCampground,
 		theSite.setUtilities(results.getBoolean("utilities"));
 		return theSite;
 	}
+
 	private Reservation mapRowToReservation(SqlRowSet results) {
 		Reservation theReservation = new Reservation();
 		theReservation.setId(results.getLong("reservation_id"));
 		theReservation.setSiteId(results.getLong("site_id"));
 		theReservation.setName(results.getString("name"));
-		theReservation
+		theReservation.setFromDate(results.getDate("from_date").toLocalDate());
+		theReservation.setToDate(results.getDate("to_date").toLocalDate());
+		theReservation.setCreateDate(results.getDate("create_date").toLocalDate());
 		return theReservation;
 	}
 
