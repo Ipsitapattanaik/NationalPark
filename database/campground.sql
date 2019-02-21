acdaed4cd078538e4830663719dc206483c1a78a
Alexandra Gales <agales136@gmail.com>
2019-02-21 11:13:03 -0500

    Putting campground.sql back to original

--- database/campground.sql
+++ database/campground.sql
@@ -17,7 +17,7 @@ CREATE SEQUENCE park_park_id_seq
 CREATE TABLE park (
   park_id integer DEFAULT nextval('park_park_id_seq'::regclass) NOT NULL,
   name varchar(80) NOT NULL,          -- Name of the park
-  state varchar(50) NOT NULL,      -- State name(s) where park is located
+  location varchar(50) NOT NULL,      -- State name(s) where park is located
   establish_date date NOT NULL,       -- Date park was established
   area integer NOT NULL,              -- Area in acres
   visitors integer NOT NULL,          -- Latest recorded number of annual visitors
@@ -76,15 +76,15 @@ CREATE TABLE reservation (
 
 
 -- Acadia
-INSERT INTO park (name, state, establish_date, area, visitors, description)
+INSERT INTO park (name, location, establish_date, area, visitors, description)
 VALUES ('Acadia', 'Maine', '1919-02-26', 47389, 2563129, 'Covering most of Mount Desert Island and other coastal islands, Acadia features the tallest mountain on the Atlantic coast of the United States, granite peaks, ocean shoreline, woodlands, and lakes. There are freshwater, estuary, forest, and intertidal habitats.');
 
 -- Arches
-INSERT INTO park (name, state, establish_date, area, visitors, description)
+INSERT INTO park (name, location, establish_date, area, visitors, description)
 VALUES ('Arches',	'Utah', '1929-04-12', 76518,	1284767, 'This site features more than 2,000 natural sandstone arches, including the famous Delicate Arch. In a desert climate, millions of years of erosion have led to these structures, and the arid ground has life-sustaining soil crust and potholes, which serve as natural water-collecting basins. Other geologic formations are stone columns, spires, fins, and towers.');
 
 -- Cuyahoga
-INSERT INTO park (name, state, establish_date, area, visitors, description)
+INSERT INTO park (name, location, establish_date, area, visitors, description)
 VALUES ('Cuyahoga Valley', 'Ohio', '2000-10-11',32860,	2189849, 'This park along the Cuyahoga River has waterfalls, hills, trails, and exhibits on early rural living. The Ohio and Erie Canal Towpath Trail follows the Ohio and Erie Canal, where mules towed canal boats. The park has numerous historic homes, bridges, and structures, and also offers a scenic train ride.');
 
 -- Acadia Campgrounds
