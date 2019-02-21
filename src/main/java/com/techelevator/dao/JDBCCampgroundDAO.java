4029a855c89f118d833175093aa261f1cbd7db43
Alexandra Gales <agales136@gmail.com>
2019-02-21 10:07:24 -0500

    implemented DAOs

--- src/main/java/com/techelevator/dao/JDBCCampgroundDAO.java
+++ src/main/java/com/techelevator/dao/JDBCCampgroundDAO.java
@@ -1,5 +1,15 @@
 package com.techelevator.dao;
 
-public class JDBCCampgroundDAO {
+import java.util.List;
+
+import com.techelevator.model.Campground;
+
+public class JDBCCampgroundDAO implements CampgroundDAO{
+
+	@Override
+	public List<Campground> getCampgroundsByParkID(int parkID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/main/java/com/techelevator/dao/JDBCSiteDAO.java
+++ src/main/java/com/techelevator/dao/JDBCSiteDAO.java
@@ -1,5 +1,22 @@
 package com.techelevator.dao;
 
-public class JDBCSiteDAO {
+import java.time.LocalDate;
+import java.util.List;
+
+import com.techelevator.model.Site;
+
+public class JDBCSiteDAO implements SiteDAO {
+
+	@Override
+	public List<Site> getAvailableSitesByCampground(int campground_id, LocalDate start_date, LocalDate end_date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Site> getAvailableSitesByPark(int park_id, LocalDate start_date, LocalDate end_date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
