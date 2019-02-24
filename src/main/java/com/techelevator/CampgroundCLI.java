d399b60a7cff770ae9ff28b97a18a901ad102e31
Alexandra Gales <agales136@gmail.com>
2019-02-24 17:21:23 -0500

    changed datatype

--- src/main/java/com/techelevator/CampgroundCLI.java
+++ src/main/java/com/techelevator/CampgroundCLI.java
@@ -13,8 +13,6 @@ import java.util.Scanner;
 
 import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.springframework.jdbc.support.rowset.SqlRowSet;
-
 import com.techelevator.dao.CampgroundDAO;
 import com.techelevator.dao.JDBCCampgroundDAO;
 import com.techelevator.dao.JDBCParkDAO;
@@ -359,6 +357,7 @@ public class CampgroundCLI {
 			try {
 				returnedId = reserveData.createReservation(siteID, resName, fromDate, toDate);
 				System.out.println("The reservation has been made and the confirmation id is " + returnedId);
+				
 			} catch (InvalidKeyException e) {
 				System.out.println("Something went wrong when trying to create the reservation");
 			}
